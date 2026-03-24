7c0fe15a4ca98ff1e7b6aaf03b1f36c6cf78c2a9
Arman Khatamsaz <armankhatamsaz1378@gmail.com>
2026-03-24 15:02:06 +0100

    deploy qa

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -37,7 +37,7 @@ module "blog_vpc" {
 module "blog_sg" {
   source      = "terraform-aws-modules/security-group/aws"
   version     = "5.3.1"
-  name        = "blog"
+  name        = "${var.environment.name}-blog"
   description = "new security group replaced by module"
   
   vpc_id = module.blog_vpc.vpc_id
@@ -53,7 +53,7 @@ module "blog_sg" {
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name    = "blog-alb"
+  name    = "${var.environment.name}-blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
 
@@ -75,7 +75,7 @@ module "blog_alb" {
 }
 
 resource "aws_lb_target_group" "blog" {
-  name     = "blog"
+  name     = "${var.environment.name}-blog"
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.blog_vpc.vpc_id
@@ -85,20 +85,20 @@ module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.2.0"
   
-  name = "blog"
+  name = "${var.environment.name}-blog"
 
   min_size = var.min_size
   max_size = var.max_size
 
   vpc_zone_identifier         = module.blog_vpc.public_subnets
 
-  launch_template_name        = "blog"
+  launch_template_name        = "${var.environment.name}-blog"
   security_groups             = [module.blog_sg.security_group_id]
   instance_type               = var.instance_type
   image_id                    = data.aws_ami.app_ami.id
 
   traffic_source_attachments  = {
-    blog_alb = {
+    ${var.environment.name}-blog_alb = {
       traffic_source_identifier = aws_lb_target_group.blog.arn
     }
   }
